cf1d9cb16965f64e8082dcfbdb8a700587f49af8
WanZhengyan <zhengyanwan066@gmail.com>
2025-05-06 17:51:24 +0800

    Delete ALASSO.R

--- ALASSO.R
+++
@@ -1,9 +0,0 @@
-alasso<-function(X=Data_X,Y=Data_Y,intercept=F){
-  pf<-adaptive.weights(x=X,y=Y,weight.method="univariate")$weights
-  alasso_fit<-cv.glmnet(x = X,y = Y,family = "gaussian", alpha=1,penalty.factor=pf,standardize=F,intercept=intercept)
-  beta<-as.vector(coef(alasso_fit,s="lambda.min"))
-  if(intercept==F){
-    beta<-beta[-1]
-  }
-  return(beta)
-}
